0894c66b4e91a956b1277d70d627acefd0a1430f
abaez8031 <abaez8031@gmail.com>
2023-02-06 16:04:58 -0500

    working on hashset

--- hashmap_lru_cache/skeleton/lib/p03_hash_set.rb
+++ hashmap_lru_cache/skeleton/lib/p03_hash_set.rb
@@ -1,15 +1,21 @@
 class HashSet
-  attr_reader :count
+  attr_reader :count, :num_buckets
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
+    @num_buckets = num_buckets
   end
 
   def insert(key)
+    @store[key.hash] << key
   end
 
   def include?(key)
+    @store.include?(key)
+    unless insert(key)
+      return false
+    end
   end
 
   def remove(key)
@@ -18,6 +24,7 @@ class HashSet
   private
 
   def [](num)
+    @store[num % @num_buckets]
     # optional but useful; return the bucket corresponding to `num`
   end
 
